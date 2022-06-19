21b7ecb49ef488e7034140c01984a2c67e1b890d
Dushyant Nagpure <52662739+dn-flixee@users.noreply.github.com>
2022-06-19 22:05:26 +0530

    Delete keys.h

--- include/keys.h
+++
@@ -1,8 +0,0 @@
-#define BLYNK_PRINT Serial
-#define BLYNK_TEMPLATE_ID "TMPLtzpASpbm"
-#define BLYNK_DEVICE_NAME "Quickstart Template"
-#define BLYNK_AUTH_TOKEN "QXVmA460RaCSPsUgVLd6kAPSV_wO0Y2f"
-#define WIFI_SSID ""
-#define WIFI_PASSWORD ""
-#define FIREBASE_API_KEY "AIzaSyBheESWt__L5m6kgypXcxXXeU8JLmgRBHg" 
-#define FIREBASE_DATABASE_URL "https://ultrasound-esp-c7e8e-default-rtdb.asia-southeast1.firebasedatabase.app/" 
